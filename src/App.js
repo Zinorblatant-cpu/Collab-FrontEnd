7efb9d738045e8cfc86a909a6f53e357ac8343f2
Zinorblatant-cpu <leonardolopesoliveira851@gmail.com>
2025-10-13 21:24:38 -0300

    new inplemetation with tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  <body className='min-h-screen bg-slate-50 dark:bg-zinc-900 dark:text-white'>
+    <header className='text-white sticky top-0 z-10 bg-zinc-900'>
+      <section className='max-w-6xl mx-auto py-6 px-6 flex justify-between items-center'>
+        <h1 className='text-3xl font-bold'>
+          <a href='#hero'>COLL<span className='text-amber-500'>AB</span></a>
+        </h1>
+        <button className='bg-amber-500 px-8 py-2 rounded-lg text-black font-bold whitespace-nowrap'>
+          Começar Agora
+        </button>
+      </section>
+    </header>
+    <hr className='border-t border-gray-700 mt-1' />
+    <main className='max-w-4xl mx-auto flex justify-center mt-6'>
+      <div className='bg-yellow-500/50 rounded-full px-3 py-1 flex items-center gap-3 mt-6'>
+        <p className='text-yellow-500 text-sm font-medium whitespace-nowrap'>
+          Curso Completo
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    </main>
+
+  </body>  
+);
 }
 
 export default App;
