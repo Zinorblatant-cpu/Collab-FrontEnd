0b3775491fadd188e299100970b3df0307fe977b
zinorblatant <leonardolopesoliveira851@gmail.com>
2025-11-26 02:35:29 -0300

    Reformo the end points and start HomePage.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import SalePage from './pages/SalePage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/Sale" element={<SalePage />} />
       </Routes>
     </Router>
   );
