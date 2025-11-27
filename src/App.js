f1ef339c14c6d824a43e92be0727aa43310b13c3
zinorblatant <leonardolopesoliveira851@gmail.com>
2025-11-27 14:48:40 -0300

    link oters page in homePage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import SalePage from './pages/SalePage';
+import ClassPage from './pages/ClassPage';
+import UserPage from './pages/UserPage';
+import ConfigPage from './pages/ConfigPage';
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/Sale" element={<SalePage />} />
+        <Route path='/class' element={<ClassPage/>}/>
+        <Route path='/user' element={<UserPage/>}/>
+        <Route path='/config' element={<ConfigPage/>}/>
       </Routes>
     </Router>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,75 +1,94 @@
-
+import { Link } from 'react-router-dom';
 function App() {
 
   return (
-  <body id='hero' className='min-h-screen bg-slate-50 dark:bg-zinc-900/100 dark:text-white'>
-    <main className='max-w-6xl mx-auto mt-10'>
+    
+    <body id='hero' className='min-h-screen bg-slate-50 dark:bg-zinc-900/100 dark:text-white'>
+
       
-      <section className="fixed left-0 top-0 h-screen w-20 bg-black flex flex-col justify-between items-center py-10 outline outline-yellow-500">
+      <main className='max-w-6xl mx-auto mt-10'>
 
         
-        <div>
-          <div className="bg-yellow-500 w-12 h-12 flex items-center justify-center rounded-xl text-black">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-              viewBox="0 0 24 24" fill="none" stroke="currentColor"
-              strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-              className="lucide lucide-house w-6 h-6">
-              <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
-              <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-            </svg>
+        <section className="fixed left-0 top-0 h-screen w-20 bg-black flex flex-col justify-between items-center py-10 outline outline-yellow-500">
+
+       
+          <div>
+           
+            <Link to="/">
+            <button className="bg-yellow-500 w-12 h-12 flex items-center justify-center rounded-xl text-black">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                className="lucide lucide-house w-6 h-6">
+                <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8">
+                  
+                </path>
+                <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+              </svg>
+            </button>
+            </Link>
+
+            <Link to="/class">       
+            <button className="bg-zinc-800 w-12 h-12 flex items-center justify-center rounded-xl mt-10">
+      
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                className="lucide lucide-book w-6 h-6">
+                <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20"></path>
+              </svg>
+            </button>
+            </Link>
           </div>
 
-          <div className="bg-zinc-800 w-12 h-12 flex items-center justify-center rounded-xl mt-10">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-              viewBox="0 0 24 24" fill="none" stroke="currentColor"
-              strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-              className="lucide lucide-book w-6 h-6">
-              <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20"></path>
-            </svg>
+         
+          <div>
+            <Link to="/config">
+            <button className="bg-zinc-800 w-12 h-12 flex items-center justify-center rounded-xl">
+              
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                className="lucide lucide-settings w-6 h-6">
+                <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"/>
+                <circle cx="12" cy="12" r="3"/>
+              </svg>
+            </button>
+            </Link>
+            <Link to="/user">
+            <button className="bg-zinc-800 w-12 h-12 flex items-center justify-center rounded-xl mt-10">
+           
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                className="lucide lucide-user w-6 h-6">
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </svg>
+            </button>
+            </Link>
           </div>
-        </div>
+
+        </section>
+
+        <section className="flex justify-between items-center">
+          <div>
+            <h1 className="sm:text-1xl md:text-2xl lg:text-4xl xl:text-6xl font-bold text-white whitespace-nowrap">
+              Bem-vindo ao <span className='text-amber-500'>COLLAB</span>
+            </h1>
+
+           
+            <p className='mt-4 text-sm md:text-base lg:text-xl text-gray-300 max-w-2xl mx-auto'>
+              Continue sua jornada de aprendizado
+            </p>
+          </div>
+        </section>
+
+      
+        <section className="mt-40 flex gap-6 justify-center ">
 
         
-        <div>
-          <div className="bg-zinc-800 w-12 h-12 flex items-center justify-center rounded-xl">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-              viewBox="0 0 24 24" fill="none" stroke="currentColor"
-              strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-              className="lucide lucide-settings w-6 h-6">
-              <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"/>
-              <circle cx="12" cy="12" r="3"/>
-            </svg>
-          </div>
-
-          <div className="bg-zinc-800 w-12 h-12 flex items-center justify-center rounded-xl mt-10">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-              viewBox="0 0 24 24" fill="none" stroke="currentColor"
-              strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-              className="lucide lucide-user w-6 h-6">
-              <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
-              <circle cx="12" cy="7" r="4"></circle>
-            </svg>
-          </div>
-        </div>
-
-      </section>
-
-      <section className="flex justify-between items-center">
-        <div>
-          <h1 className="sm:text-1xl md:text-2xl lg:text-4xl xl:text-6xl font-bold text-white whitespace-nowrap">
-            Bem-vindo ao <span className='text-amber-500'>COLLAB</span>
-          </h1>
-
-          <p className='mt-4 text-sm md:text-base lg:text-xl text-gray-300 max-w-2xl mx-auto'>
-            Continue sua jornada de aprendizado
-          </p>
-        </div>
-
-      </section>
-
-      <section className="mt-40 flex gap-6 justify-center ">
-        {/* Card 1 */}
-        <div className="bg-zinc-800/70  backdrop-blur-xs w-[320px] h-[160px] flex flex-col justify-center  rounded-xl focus:outline-2 outline-white">
+          <div className="bg-zinc-800/70 backdrop-blur-xs w-[320px] h-[160px] flex flex-col justify-center rounded-xl focus:outline-2 outline-white">
 
           <div className="ms-6">
             <div className="flex gap-20 items-center ">
@@ -85,7 +104,7 @@ function App() {
           
         </div>
 
-        {/* Card 2 */}
+      
         <div className="bg-zinc-800/70 w-[320px] h-[160px] flex flex-col justify-center rounded-xl">
 
           <div className="ms-6">
@@ -102,7 +121,6 @@ function App() {
 
         </div>
 
-        {/* Card 3 */}
         <div className="bg-zinc-800/70 w-[320px] h-[160px] flex flex-col justify-center  rounded-xl">
 
           <div className="ms-6">
@@ -116,7 +134,6 @@ function App() {
           </div>
         </div>
 
-        {/* Card 4 */}
         <div className="bg-yellow-500 w-[320px] h-[160px] flex flex-col justify-center  rounded-xl">
           <div className="ms-6">
             <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-[#151515]" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
