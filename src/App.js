5c07e705343a24997e5eb795bdbf706610827726
Zinorblatant-cpu <leonardolopesoliveira851@gmail.com>
2026-03-03 14:47:22 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import SalePage from './pages/SalePage';
-import ClassPage from './pages/ClassPage';
-import UserPage from './pages/UserPage';
-import ConfigPage from './pages/ConfigPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/Sale" element={<SalePage />} />
-        <Route path='/class' element={<ClassPage/>}/>
-        <Route path='/user' element={<UserPage/>}/>
-        <Route path='/config' element={<ConfigPage/>}/>
       </Routes>
     </Router>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,164 +1,81 @@
 import { Link } from 'react-router-dom';
+
 function App() {
-
   return (
-    
-    <body id='hero' className='min-h-screen bg-slate-50 dark:bg-zinc-900/100 dark:text-white'>
-
-      
-      <main className='max-w-6xl mx-auto mt-10'>
-
-        
-        <section className="fixed left-0 top-0 h-screen w-20 bg-black flex flex-col justify-between items-center py-10 outline outline-yellow-500">
-
-       
-          <div>
-           
-            <Link to="/">
-            <button className="bg-yellow-500 w-12 h-12 flex items-center justify-center rounded-xl text-black">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                className="lucide lucide-house w-6 h-6">
-                <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8">
-                  
-                </path>
-                <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-              </svg>
-            </button>
-            </Link>
-
-            <Link to="/class">       
-            <button className="bg-zinc-800 w-12 h-12 flex items-center justify-center rounded-xl mt-10">
-      
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                className="lucide lucide-book w-6 h-6">
-                <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20"></path>
-              </svg>
-            </button>
-            </Link>
-          </div>
-
-         
-          <div>
-            <Link to="/config">
-            <button className="bg-zinc-800 w-12 h-12 flex items-center justify-center rounded-xl">
-              
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                className="lucide lucide-settings w-6 h-6">
-                <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"/>
-                <circle cx="12" cy="12" r="3"/>
-              </svg>
-            </button>
-            </Link>
-            <Link to="/user">
-            <button className="bg-zinc-800 w-12 h-12 flex items-center justify-center rounded-xl mt-10">
-           
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                className="lucide lucide-user w-6 h-6">
-                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
-                <circle cx="12" cy="7" r="4"></circle>
-              </svg>
-            </button>
-            </Link>
-          </div>
-
-        </section>
-
-        <section className="flex justify-between items-center">
-          <div>
-            <h1 className="sm:text-1xl md:text-2xl lg:text-4xl xl:text-6xl font-bold text-white whitespace-nowrap">
-              Bem-vindo ao <span className='text-amber-500'>COLLAB</span>
-            </h1>
-
-           
-            <p className='mt-4 text-sm md:text-base lg:text-xl text-gray-300 max-w-2xl mx-auto'>
-              Continue sua jornada de aprendizado
-            </p>
-          </div>
-        </section>
-
-      
-        <section className="mt-40 flex gap-6 justify-center ">
-
-        
-          <div className="bg-zinc-800/70 backdrop-blur-xs w-[320px] h-[160px] flex flex-col justify-center rounded-xl focus:outline-2 outline-white">
-
-          <div className="ms-6">
-            <div className="flex gap-20 items-center ">
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-[#FFB800]" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                <path d="M12 7v14"></path>
-                <path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"></path>
-              </svg>
-            </div>
-
-            <h3 className="text-xl font-semibold mt-2">4</h3>
-            <p className="text-sm text-zinc-300">Cursos Disponíveis</p>
-          </div>
+    <div id="hero" className="min-h-screen bg-slate-50 dark:bg-zinc-900 dark:text-white">
+      <main className="max-w-6xl mx-auto mt-10">
+        <header className="flex justify-between items-center sticky top-0 z-10 backdrop-blur-sm py-4">
           
-        </div>
+          <h1 className="text-2xl font-bold">
+            <a href="#hero">Prandianinhos.com</a>
+          </h1>
 
-      
-        <div className="bg-zinc-800/70 w-[320px] h-[160px] flex flex-col justify-center rounded-xl">
-
-          <div className="ms-6">
-            <div className="">
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-blue-400" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                <circle cx="12" cy="12" r="10"></circle>
-                <polyline points="12 6 12 12 16 14"></polyline>
-              </svg>
-            </div>
-
-            <h3 className="text-2xl font-semibold mt-2">4</h3>
-            <p className="text-sm text-zinc-300">Cursos Disponíveis</p>
-          </div>
-
-        </div>
-
-        <div className="bg-zinc-800/70 w-[320px] h-[160px] flex flex-col justify-center  rounded-xl">
-
-          <div className="ms-6">
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-purple-400" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-              <path d="m15.477 12.89 1.515 8.526a.5.5 0 0 1-.81.47l-3.58-2.687a1 1 0 0 0-1.197 0l-3.586 2.686a.5.5 0 0 1-.81-.469l1.514-8.526"></path>
-              <circle cx="12" cy="8" r="6"></circle>
+          {/* Github Button */}
+          <button
+            className="
+              group relative flex items-center justify-center
+              w-[45px] h-[45px]
+              bg-[rgb(31,31,31)]
+              rounded-full
+              overflow-hidden
+              transition-all duration-400
+              hover:w-[110px]
+              hover:rounded-[30px]
+            "
+          >
+            {/* SVG */}
+            <svg
+              className="h-[1.4em] transition-opacity duration-300 group-hover:opacity-0"
+              viewBox="0 0 496 512"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill="white"
+                d="M244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2
+                12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4
+                0 0-70 15-84.7-29.8
+                0 0-11.4-29.1-27.8-36.6
+                0 0-22.9-15.7 1.6-15.4
+                0 0 24.9 2 38.6 25.8
+                21.9 38.6 58.6 27.5 72.9 20.9
+                2.3-16 8.8-27.1 16-33.7
+                -55.9-6.2-112.3-14.3-112.3-110.5
+                0-27.5 7.6-41.3 23.6-58.9
+                -2.6-6.5-11.1-33.3 2.6-67.9
+                20.9-6.5 69 27 69 27
+                20-5.6 41.5-8.5 62.8-8.5
+                s42.8 2.9 62.8 8.5
+                c0 0 48.1-33.6 69-27
+                13.7 34.7 5.2 61.4 2.6 67.9
+                16 17.7 25.8 31.5 25.8 58.9
+                0 96.5-58.9 104.2-114.8 110.5
+                9.2 7.9 17 22.9 17 46.4
+                0 33.7-.3 75.4-.3 83.6
+                0 6.5 4.6 14.4 17.3 12.1
+                C428.2 457.8 496 362.9 496 252
+                496 113.3 383.5 8 244.8 8z"
+              />
             </svg>
-          
-            <h3 className="text-xl font-semibold mt-2">4</h3>
-            <p className="text-sm text-zinc-300">Cursos Disponíveis</p>
-          </div>
-        </div>
 
-        <div className="bg-yellow-500 w-[320px] h-[160px] flex flex-col justify-center  rounded-xl">
-          <div className="ms-6">
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-[#151515]" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-              <polygon points="6 3 20 12 6 21 6 3"></polygon>
-            </svg>
-          
-            <h3 className="text-xl font-semibold mt-2">4</h3>
-            <p className="text-sm text-black">Cursos Disponíveis</p>
-          </div>
-        </div>
-      </section>
+            {/* Text */}
+            <span
+              className="
+                absolute
+                text-white
+                font-semibold
+                opacity-0
+                transition-opacity duration-400
+                group-hover:opacity-100
+              "
+            >
+              Github
+            </span>
+          </button>
 
-      <section className="mt-40 flex gap-6  ">
-        <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-white ">
-            Seus Cursos
-        </h1>
-
-        <div className="bg-gray-600;">
-
-        </div>
-      </section>
-    </main>
-  </body>
-
-);
+        </header>
+      </main>
+    </div>
+  );
 }
 
 export default App;
